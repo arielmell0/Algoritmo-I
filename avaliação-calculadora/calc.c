4497024e1d2cfbeeb6f45602693258f03075587d
arielmell0 <arielmello@rede.ulbra.br>
2021-07-02 13:35:33 -0300

    feat: :sparkles: do while repeat program loop
    repeat the calc program when the final input is == 0
    
    float number division module closed using the fmod function of the math.h library

--- avaliação-calculadora/calc.c
+++ avaliação-calculadora/calc.c
@@ -7,59 +7,62 @@
 
 #include <stdio.h>
 #include <locale.h>
+#include <math.h> // Uso da função fmod() para módulo de tipo float
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
     float valorA, valorB, resposta;
-    int operacao, modA, modB;
+    int operacao, exit;
 
-    printf("-----------------------------------------\n");
-    printf("1: Soma\n");
-    printf("2: Subtração\n");
-    printf("3: Multiplicação\n");
-    printf("4: Divisão\n");
-    printf("5: Resto da divisão\n");
-    printf("-----------------------------------------\n");
-    printf("Digite o número da operação que deseja realizar: ");
-    scanf("%i", &operacao);
-    printf("Digite o primeiro valor: ");
-    scanf("%f", &valorA);
-    printf("Digite o segundo valor: ");
-    scanf("%f", &valorB);
+    do{
+        printf("-----------------------------------------\n");
+        printf("1: Soma\n");
+        printf("2: Subtração\n");
+        printf("3: Multiplicação\n");
+        printf("4: Divisão\n");
+        printf("5: Resto da divisão\n");
+        printf("-----------------------------------------\n");
+        printf("Digite o número da operação que deseja realizar: ");
+        scanf("%i", &operacao); 
+        printf("Digite o primeiro valor: ");
+        scanf("%f", &valorA);
+        printf("Digite o segundo valor: ");
+        scanf("%f", &valorB);
 
-    modA = (int)valorA;
-    modB = (int)valorB;
+        switch(operacao){
+            case 1:
+                resposta = valorA + valorB;
+                printf("%.2f + %.2f = %.2f\n\n", valorA, valorB, resposta);
+            break;
 
-    switch(operacao){
-        case 1:
-            resposta = valorA + valorB;
-            printf("%.2f + %.2f = %.2f", valorA, valorB, resposta);
-        break;
+            case 2:
+                resposta = valorA - valorB;
+                printf("%.2f - %.2f = %.2f\n\n", valorA, valorB, resposta);
+            break;
 
-        case 2:
-            resposta = valorA - valorB;
-            printf("%.2f - %.2f = %.2f", valorA, valorB, resposta);
-        break;
+            case 3:
+                resposta = valorA * valorB;
+                printf("%.2f * %.2f = %.2f\n\n", valorA, valorB, resposta);
+            break;
 
-        case 3:
-            resposta = valorA * valorB;
-            printf("%.2f * %.2f = %.2f", valorA, valorB, resposta);
-        break;
+            case 4:
+                resposta = valorA / valorB;
+                printf("%.2f / %.2f = %.2f\n\n", valorA, valorB, resposta);
+            break;      
 
-        case 4:
-            resposta = valorA / valorB;
-            printf("%.2f / %.2f = %.2f", valorA, valorB, resposta);
-        break;      
+            case 5:
+                resposta = fmod(valorA, valorB);
+                printf("%.2f mod %.2f = %.2f\n\n", valorA, valorB, resposta);
+            break;
 
-        case 5:
-            resposta = (int)valorA % (int)valorB;
-            printf("%i mod %i = %i", valorA, valorB, resposta);
-        break;
+            default:
+                printf("Erro!\n\n");
+            break;
+        }
 
-        default:
-            printf("Erro!\n");
-        break;
-    }
+        printf("Digite 0 para repetir a operação: ");
+        scanf("%i", &exit);
+    } while (exit == 0);
 
     return 0;
 }
